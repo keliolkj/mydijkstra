9b7c4128db08ea49af803f8213f17225760a2f62
keliolkj <149433111+keliolkj@users.noreply.github.com>
2023-11-05 21:57:30 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,27 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# Insert your PriorityQueue class definition here
+# Sample PriorityQueue implementation (Replace this with your actual implementation)
 class PriorityQueue:
-    # Constructor and other methods with proper indentation
-    # ...
+    def __init__(self):
+        self.elements = []
+    
+    def is_empty(self):
+        return len(self.elements) == 0
+    
+    def put(self, item, priority):
+        self.elements.append((priority, item))
+    
+    def get(self):
+        self.elements.sort()  # This should be a heap in a proper implementation
+        return self.elements.pop(0)[1]
 
-# Insert your myDijkstra function here, with proper indentation
-def myDijkstra(adj_matrix, origin):
-    # Your Dijkstra's algorithm implementation
-    # ...
+# Sample Dijkstra's algorithm implementation (Replace this with your actual implementation)
+def myDijkstra(graph, start):
+    # This is a placeholder implementation. Replace it with your actual algorithm.
+    return {}, {}
 
-@app.route('/shortest-path')
+@app.route('/shortest-path', methods=['GET'])
 def shortest_path():
     origin = request.args.get('origin', type=int)
     destination = request.args.get('destination', type=int)
@@ -20,23 +30,25 @@ def shortest_path():
     if origin is None or destination is None:
         return jsonify({"error": "Origin and destination parameters are required."}), 400
 
-    # Assuming adj_matrix is globally defined or you have a way to get it based on origin and destination
-    adj_matrix = # ... (your code to generate or get the adjacency matrix)
+    # Placeholder for the graph data structure (Replace with your actual graph)
+    graph = {
+        # ... your graph data ...
+    }
 
-    # Get the shortest path from the Dijkstra's algorithm
-    dist, prev = myDijkstra(adj_matrix, origin)
-    
-    # Reconstruct the path from the destination to the origin
+    # Run Dijkstra's algorithm
+    dist, prev = myDijkstra(graph, origin)
+
+    # Reconstruct the shortest path
     path = []
     current_node = destination
-    while current_node is not None and current_node != origin:
+    while current_node != origin:
+        if current_node is None:
+            return jsonify({"error": "No path found from origin to destination"}), 404
         path.insert(0, current_node)
-        current_node = prev[current_node]
-    if current_node is None:
-        return jsonify({"error": "No path found from origin to destination"}), 404
+        current_node = prev.get(current_node, None)
     path.insert(0, origin)
     
     return jsonify({"shortest_path": path})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=70, debug=True)  # Running on all interfaces on port 80, not recommended for production
+    app.run(debug=True)  # Set debug=False for production use
