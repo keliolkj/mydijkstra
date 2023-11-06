e2bdb7ade28100178a1218e130e0dd862efa4d5f
keliolkj <149433111+keliolkj@users.noreply.github.com>
2023-11-05 21:40:25 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,11 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 
-# Placeholder for the Dijkstra's algorithm implementation
-def dijkstra_algorithm(origin, destination):
-    # This function will hold the Dijkstra's algorithm to compute the shortest path.
-    # It will return a dummy response for now.
-    return [origin, "some_other_node", destination]
-
-# Initialize the Flask application
 app = Flask(__name__)
 
-# Define a route for the API
-@app.route('/shortest-path', methods=['GET'])
-def shortest_path():
-    # Get parameters from the URL
-    origin = request.args.get('origin', default='', type=str)
-    destination = request.args.get('destination', default='', type=str)
+@app.route('/')
+def hello_world():
+    result = 'Hello, World!'
+    return f"<pre>{result}</pre>"
 
-    # Check if both parameters are provided
-    if not origin or not destination:
-        return jsonify({"error": "Missing origin or destination parameter"}), 400
-
-    # Here we would call the actual Dijkstra's algorithm function
-    path = dijkstra_algorithm(origin, destination)
-
-    # Return the path as JSON
-    return jsonify({"shortest_path": path})
-
-# This is only used when running locally and will not work in a production environment
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=70)
+    app.run(debug=True, host='0.0.0.0', port=80)
